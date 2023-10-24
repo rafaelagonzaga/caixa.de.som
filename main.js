a92aada1feb72432f0f5fe2c4b699322c9a3d5d1
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2023-10-24 09:04:30 -0300

    ok

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ function tocaSomPom(){
 //chamando/invocando a função tocaSomPom pelo clique do botão
 document.querySelector('.tecla_pom').onclick =tocaSomPom;
 
-
 //referência constante listaDeTeclas, buscando todos o botões 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSomPom pelo item "0" da listaDeTecla
 listaDeTeclas[1].onclick = tocaSomClap
 
-while(){
     let contador = 0
+    while(contador <9){
+        listaDeTeclas{contador}.onclick = tocaSomPom;
 }
