89095fc25e2ff37998499a4ef59665906d0e919c
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2023-10-24 09:24:10 -0300

    ok,

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[1].onclick = tocaSomPom
 
     let contador = 0
-    while(contador <9){
+    while(contador < listaDeTeclas.length){
      listaDeTeclas[contador].onclick = tocaSomPom;
      contador = contador + 1;
      console.log(contador);
