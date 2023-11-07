b6d7eeb53e9360543ca25e53f8f669de4ea6a61b
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2023-11-07 09:17:59 -0300

    ok

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ listaDeTeclas[0].onclick = tocaSom
 
     let contador = 0
     while(contador < listaDeTeclas.length){
-        const efeito = listaDeTeclas[contador].classList[1];
+        const tecla = listaDeTeclas[contador]
+        const efeito =  tecla.classList[1];
         const idAudio = "#som_"+ efeito;
-     listaDeTeclas[contador].onclick = function(){
+      tecla.onclick = function(){
        tocaSom(Audio)
      };
      contador = contador + 1;
-     console.log(contador);
 }
