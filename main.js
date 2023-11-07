acecbc060add22ffebafbe37530d74420f903e47
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2023-11-07 09:08:25 -0300

    ok

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ document.querySelector('.tecla_pom').onclick =tocaSom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSom pelo item "0" da listaDeTecla
-listaDeTeclas[1].onclick = tocaSom
+listaDeTeclas[0].onclick = tocaSom
 
     let contador = 0
     while(contador < listaDeTeclas.length){
         const efeito = listaDeTeclas[contador].classList[1];
         const idAudio = "#som_"+ efeito;
      listaDeTeclas[contador].onclick = function(){
-
+       tocaSom(Audio)
      };
      contador = contador + 1;
      console.log(contador);
